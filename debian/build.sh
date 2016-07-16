96f6d03da5e629271c207cdae88e5a66e5c4b745
Michael Peick <michael.peick@gmail.com>
2016-07-16 14:21:03 +0200

    Added debian package building.

---
+++ debian/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+cd `readlink -f $0`/../../..
+rsync -av --exclude knockknock/.tox --exclude "*.pyc" knockknock /tmp
+
+cd /tmp/knockknock
+rm /tmp/knockknock*.deb
+dpkg-buildpackage -uc -us
+
+sudo dpkg -i /tmp/knockknock*.deb
